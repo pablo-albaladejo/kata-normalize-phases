954e13632496f0e5affc727c3bf0d03230218707
kevin Martínez <kevinccbsg@gmail.com>
2020-05-08 16:04:47 +0200

    fix: change test to jest

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
-const expect = require('expect.js');
 const kata = require('./kata.js');
 
 describe('is valid output', () => {
   it('is not null', () => {
     const result = kata.run();
-    expect(result).to.not.equal(null);
+    expect(result).notEqual(null);
   });
 })
