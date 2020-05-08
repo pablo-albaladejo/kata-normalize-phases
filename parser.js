4ef980c3f7e49a0fa953a40f93bb7b84bf59ec66
Ismael Bakkali <ismael.bakkali@guidesmiths.com>
2020-05-08 16:54:16 +0200

    Remove special accents

--- parser.js
+++ parser.js
@@ -3,14 +3,22 @@ const config = require("./config.json");
 module.exports = ({ phrase, locale = "ES" }) => {
   if (phrase == "") return [];
 
-  const localePrepositions = config[locale] && config[locale].prepositions;
+  const { prepositions, pluralsRegx } = config[locale];
+
+  const localePrepositions = prepositions;
 
   const removePrepositions = (word) => !localePrepositions.includes(word);
+
+  const fromPluralToSingular = (word) => {
+    const removeS = new RegExp(pluralsRegx, "g");
+    return word.replace(removeS, "");
+  };
 
   return phrase
     .normalize("NFD")
     .replace(/[^a-zA-Z ]/g, "")
     .toUpperCase()
     .split(" ")
-    .filter(removePrepositions);
+    .filter(removePrepositions)
+    .map(fromPluralToSingular);
 };
